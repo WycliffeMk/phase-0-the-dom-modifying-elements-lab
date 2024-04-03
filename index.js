70179bd0b18e66ab8087b7bce81accd60c350142
Wycliffe Munyes <wyclefmunyes@gmail.com>
2024-04-03 10:30:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+// console.log(newHeader);
+// newHeader.id = "victory"
+// newHeader.textContent = "Wycliffe Emuria is a champion";
+
+const newHeader =document.getElementById('victory');
+console.log(newHeader);
